82bad7ccc86e88a36801b294fd70d5b0aa17e0d2
Nibras Abo Alzahab <nibras.abo.alzahab@gmail.com>
2020-08-09 08:53:52 +0200

    Spikes Removal
    Methode of Spikes Removal was changed.
    The range of quantile was expanded to keep more useful information in the signal

--- EEG_OCeye_With_PreProcessing.py
+++ EEG_OCeye_With_PreProcessing.py
@@ -3,14 +3,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import mne
+from scipy import signal 
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report,confusion_matrix,accuracy_score,roc_curve,auc, precision_score,recall_score,f1_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-
-
-
 
 #Dataset Reading
 Data = pd.read_csv("eeg.csv") 
@@ -30,8 +28,7 @@ print(Data['eye'].head())
 Data['eye'].value_counts().plot.pie(labels = ["1-open","0-closed"],
                                               autopct = "%1.0f%%",
                                               shadow = True,explode=[0,.1])
-
-
+    
 #Data set Parameters
 ch_names = ['AF3', 'F7', 'F3', 'FC5', 'T7', 'P7', 'O1', 'O2', 'P8', 'T8', 'FC6', 'F4', 'F8', 'AF4', 'eye'] # channel names   
 sfreq = 128  # sampling frequency, in hertz  
@@ -41,22 +38,21 @@ info = mne.create_info(ch_names, sfreq)
 Data['eye']=Data["eye"].astype('category')
 Data["eye"] = Data["eye"].cat.codes
 
-for i in ch_names[1:14]:
+for i in ch_names[0:14]:
     plt.plot(Data[i])
 
 # Despiking
-for i in ch_names[1:14]:
-    y = Data[i]
-    removed_outliers = y.between(y.quantile(.05), y.quantile(.95))
-    #print(str(y[removed_outliers].size) + "/" + str(size) + " data points remain.") 
-    #y[removed_outliers].plot().get_figure()
-    Data[i]= y[removed_outliers]
+for i in ch_names[0:14]:
+    Data[i]=Data[i].where(Data[i] < Data[i].quantile(0.999), Data[i].mean())
+    Data[i]=Data[i].where(Data[i] > Data[i].quantile(0.001), Data[i].mean())
     plt.plot(Data[i])
     
-Data = Data.fillna(Data.mean()) #Replace Nan with mean
 
-Data['AF3'][11509] = Data['AF3'].mean() #Replace AF3 Max Spike with mean
+signal.detrend(Data[1:14], axis=- 1, type='linear', bp=0, overwrite_data=False)
 
+for i in ch_names[0:14]:
+    plt.plot(Data[i])
+    
 #Data Normalizing (min-max Scaling)
 x = Data.values #returns a numpy array
 min_max_scaler = preprocessing.MinMaxScaler()
@@ -64,11 +60,22 @@ x_scaled = min_max_scaler.fit_transform(x)
 Data = pd.DataFrame(x_scaled)
 Data.columns=ch_names
 
+for i in ch_names[0:14]:
+    plt.plot(Data[i])
+    
 #Numpy Array to mne data
 data = Data.to_numpy() # Pandas DataFrame to Numpy Array
 data_mne = np.transpose(data)
 raw = mne.io.RawArray(data_mne, info)   
 raw.plot() #Plot all the signales
+
+# Scatter plot each two channels (featers) together
+for i in ch_names[1:14]:
+    for k in ch_names[1:14]:
+        sns.lmplot(i, k, Data, hue='eye', fit_reg=False)
+        fig = plt.gcf()
+        #fig.set_size_inches(15, 10)
+        plt.show()
 
 # Prepare Train and test Data
 splitRatio = 0.2
@@ -88,28 +95,28 @@ test_Y = test["eye"]
 ## Traiing Model
 clf_DT = DecisionTreeClassifier()
 clf_DT = clf_DT.fit(train_X,train_Y)
-pred_y = clf_DT.predict(test_X)
+pred_y_DT = clf_DT.predict(test_X)
 
 ## Metrics
-print("Accuracy:",accuracy_score(test_Y, pred_y))
-print("f1 score:", f1_score(test_Y, pred_y))
-print("confusion matrix:",confusion_matrix(test_Y, pred_y))
-print("precision score:", precision_score(test_Y, pred_y))
-print("recall score:", recall_score(test_Y, pred_y))
-print("classification report:", classification_report(test_Y, pred_y))
+print("Accuracy:",accuracy_score(test_Y, pred_y_DT))
+print("f1 score:", f1_score(test_Y, pred_y_DT))
+print("confusion matrix:",confusion_matrix(test_Y, pred_y_DT))
+print("precision score:", precision_score(test_Y, pred_y_DT))
+print("recall score:", recall_score(test_Y, pred_y_DT))
+print("classification report:", classification_report(test_Y, pred_y_DT))
 
 ## Plots
 ### 01 plot Confusion Matrix as heat map
 plt.figure(figsize=(13,10))
 plt.subplot(221)
-sns.heatmap(confusion_matrix(test_Y, pred_y),annot=True,fmt = "d",linecolor="k",linewidths=3)
+sns.heatmap(confusion_matrix(test_Y, pred_y_DT),annot=True,fmt = "d",linecolor="k",linewidths=3)
 plt.title("CONFUSION MATRIX",fontsize=20)
 
 ### 02 plot ROC curve
 test_Y_01 =test_Y.astype('category')
 test_Y_01 = test_Y_01.cat.codes
 
-pred_y_01 = pd.Series(pred_y)
+pred_y_01 = pd.Series(pred_y_DT)
 pred_y_01 =pred_y_01.astype('category')
 pred_y_01 = pred_y_01.cat.codes
 
@@ -124,28 +131,28 @@ plt.title("ROC - CURVE & AREA UNDER CURVE",fontsize=15)
 ## Traiing Model
 clf_KNN = KNeighborsClassifier(15)
 clf_KNN = clf_KNN.fit(train_X,train_Y)
-pred_y = clf_KNN.predict(test_X)
+pred_y_KNN = clf_KNN.predict(test_X)
 
 ## Metrics
-print("Accuracy:",accuracy_score(test_Y, pred_y))
-print("f1 score:", f1_score(test_Y, pred_y))
-print("confusion matrix:",confusion_matrix(test_Y, pred_y))
-print("precision score:", precision_score(test_Y, pred_y))
-print("recall score:", recall_score(test_Y, pred_y))
-print("classification report:", classification_report(test_Y, pred_y))
+print("Accuracy:",accuracy_score(test_Y, pred_y_KNN))
+print("f1 score:", f1_score(test_Y, pred_y_KNN))
+print("confusion matrix:",confusion_matrix(test_Y, pred_y_KNN))
+print("precision score:", precision_score(test_Y, pred_y_KNN))
+print("recall score:", recall_score(test_Y, pred_y_KNN))
+print("classification report:", classification_report(test_Y, pred_y_KNN))
 
 ## Plots
 ### 01 plot Confusion Matrix as heat map
 plt.figure(figsize=(13,10))
 plt.subplot(221)
-sns.heatmap(confusion_matrix(test_Y, pred_y),annot=True,fmt = "d",linecolor="k",linewidths=3)
+sns.heatmap(confusion_matrix(test_Y, pred_y_KNN),annot=True,fmt = "d",linecolor="k",linewidths=3)
 plt.title("CONFUSION MATRIX",fontsize=20)
 
 ### 02 plot ROC curve
 test_Y_01 =test_Y.astype('category')
 test_Y_01 = test_Y_01.cat.codes
 
-pred_y_01 = pd.Series(pred_y)
+pred_y_01 = pd.Series(pred_y_KNN)
 pred_y_01 =pred_y_01.astype('category')
 pred_y_01 = pred_y_01.cat.codes
 
